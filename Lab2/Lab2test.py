1a54ec4608724d27f1303033981bf8743686a5a1
Your Name <par.augustsson@iths.se>
2024-10-03 12:50:41 +0200

    changes to Lab2 and .txt-files

--- Lab2/Lab2test.py
+++ Lab2/Lab2test.py
@@ -1,71 +1,113 @@
+# Use pandas library to read datapoints.txt in csv_format in two dimensions.
 import pandas as pd
-import matplotlib as plt
+import numpy as np
 
-df = pd.read_csv("datapoints.txt")
-df.columns = df.columns.str.replace("(", "").str.replace(")", "")
-df.columns = df.columns.str.strip().str.replace(" ", "_").str.capitalize()
-df["0-Pichu_1-Pikachu"] = df["Label_0-pichu"]
-df = df.drop(["Label_0-pichu","1-pikachu"], axis = 1)
+dp = pd.read_csv("datapoints.txt")
 
-ett = df[df["0-Pichu_1-Pikachu"] == 1]
-noll = df[df["0-Pichu_1-Pikachu"] == 0]
+# Clean dataframe from unnecessary symbols and white spaces  
+dp.columns = dp.columns.str.replace("(", "").str.replace(")", "")
+dp.columns = dp.columns.str.strip().str.replace(" ", "_").str.capitalize()
+
+#Create and drop columns and asign iy to a new column 
+dp["0-Pichu_1-Pikachu"] = dp["Label_0-pichu"]
+dp = dp.drop(["Label_0-pichu","1-pikachu"], axis = 1)
+
+# Display first five rows of dataframe 
+# print(dp.head())
 
 
-plt.scatter(x = ett["Width_cm"], y = ett["Height_cm"], color = "green")
-plt.scatter(x = noll["Width_cm"], y = noll["Height_cm"], color = "red")
+#Split dataset in dp into two new variables for further analysis 
+pichu = dp[dp["0-Pichu_1-Pikachu"] == 0]
+pikachu = dp[dp["0-Pichu_1-Pikachu"] == 1]
+#print(pichu)
 
+
+#Create empty list for further data collection.
 test_list = []
 
-
-# Öppna testdatan och läs in som testfil.   
-with open("testpoints.txt", "r") as test_file: # tilldela filobjeette till "test_file"
-    fil = test_file.readlines()
-    for line in fil:
+# Open and read file and collect and asign data.
+with open("testpoints.txt", "r") as test_file: 
+    test_file = test_file.readlines()
+    for line in test_file:
         test_list.append(line)
 
-#omvandla datan till en dataframe
+# Converting data into data frame and clean data  
 test_data = pd.DataFrame(test_list, columns= ["test_d"])
 test_data = test_data.drop(0, axis = 0) 
 
-#gör om data till lämlplig struktur 
+# Transforming data into proper format and store it. 
 test_data["Width_cm"] = test_data["test_d"].str.split("(").str.get(1).str.split(",").str.get(0)
 test_data["Height_cm"] = test_data["test_d"].str.split(")").str.get(0).str.split(",").str.get(1)
 test_data = test_data.drop("test_d", axis= 1)
+#print(test_data)
+
+# Skapar en funktion som beräknar test_datan och som skall få 2 inmatningar bredd och höjd.
+#import numpy as np
+
+def label_test_data(width, height):
+    dist_pikachu = []
+    dist_pichu = []
+
+    for x, y in zip(pikachu["Width_cm"], pikachu["Height_cm"]):
+        dist_pikachu.append(np.sqrt(np.power(x - width, 2) + np.power(y - height, 2))) #Eucledian distance formula  leta efter avstång mellan alla puntre per rad och kolumn för inmatat värde 
+
+    for x, y in zip(pichu["Width_cm"], pichu["Height_cm"]): #Tar bara rad oavsett om det står 2,5,7 istället för rad i numeriks ordning 
+        dist_pichu.append(np.sqrt(np.power(x - width, 2) + np.power(y - height, 2)))
+
+    shortest_pichu = min(dist_pichu)
+    shortest_pikachu = min(dist_pikachu)
+
+# om pichu är mindre än pikachu skriv pichu annars skriv pikachu 
+    if shortest_pichu < shortest_pikachu: 
+        print(f"testpoint {width, height} calssified as Pichu")
+    else: 
+        print(f"testpoint {width, height} calssified as Pikachu")
+print(label_test_data(25, 32))
 
 
-#from sklearn.model_selection import train_test_split
+def lik(xx, yy):
+   dist_dp = []
+   for x, y in zip(dp["Width_cm"], dp["Height_cm"]):
+      dist_dp.append(np.sqrt(np.power(x - xx, 2) + np.power(y - yy, 2)))
+   sort_dp = sorted(dist_dp)
+   minsta_df = sort_dp[:10]
+   index_df = []
+   for d in minsta_df:
+      index_df.append(dist_dp.index(d))
 
-#x_test = test_data[["Width_cm", "Height_cm"]]
-#x_train = df[["Width_cm", "Height_cm"]]
+   pic_pika = dp.loc[index_df, "0-Pichu_1-Pikachu"]
+   if sum(pic_pika) > 5:
+      print("pikachu")
+   else:
+      print("pichu")
 
-#y_train = df["0-Pichu_1-Pikachu"]
-
-eucledian samma princip 
-
-läs avståndet på 150 tetspunkter 
-läs 75 avstånd / läs 75 svtånd 
-
-använd av for -loopar för att räkna ut a ståndet at frma det mionsta värdet 
-
-minsta värdet är picachu eller pichu 
-
-lägg in allt i en lista för att hotta minsta värdet 
-
-
-#from sklearn.linear_model import LogisticRegression
-
-#log_reg = LogisticRegression()
-#log_reg = fit(x_train, y_train)
-
-#prediction = log_reg.predict(x_test)
-#prediction_test = log_reg.predict(test_data)
-
-# hitta en algortim (vad ska vi förutsäga)
-
-#from sklearn.metrics import confusion_matrix
-#con_prediction = confusion_matrix(y_test, prediction) #utvärdera träningsdatan med 
+#lik(19, 34)
 
 
-#While True:
+#Create algortim for user datapoint as input and handle non numerical values and negative values and feedback user....
+while True: 
+    user_width = input("Please enter a postive number for width:")
+    user_height = input("Please enter a negative number as height:") 
+    org_user_width = user_width
+    org_user_height = user_height
 
-#x_input = input("enter_")
+    user_width = user_width.replace(".", "").replace("-", "")
+    user_height = user_height.replace(".", "").replace("-", "")
+
+    if user_width.isdigit() == False or user_height.isdigit() == False:
+          print("is not a number")
+    
+    if user_width.isdigit() == True and user_height.isdigit() == True:
+         if float(org_user_width) < 0 and float(org_user_height) < 0:
+           print("is a negative number")
+         elif float(org_user_width) > 0 and float(org_user_height) > 0:
+             lik(float(org_user_width), float(org_user_height))
+             break
+
+
+
+
+
+
+
+
